528624bd9fbe18cefe146f400d28dbfe2198db74
vgmariucci <vgmariucci@hotmail.com>
2025-03-10 00:00:44 -0300

    Added the sql folder to the project

---
+++ sql/create_sensor_readings_table.sql
@@ -0,0 +1,12 @@
+-- Query to create the sensor_readings table in the iot_data database
+-- The table will store the sensor readings sent by the ESP32 devices via MQTT
+
+    CREATE TABLE sensor_readings (
+    id SERIAL PRIMARY KEY,
+    reading_time TIMESTAMPTZ NOT NULL,
+    customer_ID TEXT NOT NULL,
+    iot_device_serial_number TEXT NOT NULL,
+    temperature NUMERIC(5,2),
+    humidity NUMERIC(5,2),
+    wifi_status INTEGER
+    );
